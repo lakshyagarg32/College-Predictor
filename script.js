8abf2dbeb2bbf4b5ea446c0ff9889bc14e42e552
samm-01 <sameergarg006@gmail.com>
2022-11-17 18:12:18 +0530

    Minor changes

--- script.js
+++ script.js
@@ -204,11 +204,11 @@ prevBtnFourth.addEventListener("click", function(event){
    
    function update() {
    
-     velocity.tx *= 0.96;
-     velocity.ty *= 0.96;
+     velocity.tx *= 0.86;
+     velocity.ty *= 0.86;
    
-     velocity.x += ( velocity.tx - velocity.x ) * 0.8;
-     velocity.y += ( velocity.ty - velocity.y ) * 0.8;
+     velocity.x += ( velocity.tx - velocity.x ) * 0.05;
+     velocity.y += ( velocity.ty - velocity.y ) * 0.05;
    
      stars.forEach( ( star ) => {
    
