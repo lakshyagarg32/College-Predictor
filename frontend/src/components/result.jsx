6594aca1143268de964659f83965d43735905fe9
Lakshya <lakshyagarg13200132@gmail.com>
2022-11-29 17:52:31 +0530

    working database

--- frontend/src/components/result.jsx
+++ frontend/src/components/result.jsx
@@ -2,8 +2,13 @@ import { useLocation } from "react-router-dom";
 
 function Result() {
   const { state } = useLocation();
-  const { rank, gender, category, domicile_state } = state; // Read values passed on state
-  return <h1>{rank} {gender} {category} {domicile_state} </h1>;
+  const { rank, gender, category, domicile_state, result } = state; // Read values passed on state
+  console.log(result);
+  return (
+    <h1>
+      {rank} {gender} {category} {domicile_state}{" "}
+    </h1>
+  );
 }
 
 export default Result;
